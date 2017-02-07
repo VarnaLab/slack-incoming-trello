915cbbbda7a7ee9682abeeeaa1cec206b605adf7
simov <simeonvelichkov@gmail.com>
2017-02-07 22:53:37 +0200

    Move the console logging to the cli module

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,10 @@ var hook = require('../')
 
 hook.init({})
 hook.check()
+  .then(([res, body]) =>
+    console.log(new Date().toString(), res.statusCode, body))
+  .catch((err) =>
+    console.error(new Date().toString(), err))
 
 // test single attachment
 // hook.post(config, [attachment(require('../config/fixture'))])
